689852bc1e634f8c1ea136682f801420519f1ee2
乐哈哈 <150517295+Wangin1996@users.noreply.github.com>
2025-03-19 12:03:02 +0800

    Update DYYYManager.m

--- DYYYManager.m
+++ DYYYManager.m
@@ -5,6 +5,7 @@
 #import <ImageIO/ImageIO.h>
 #import <MobileCoreServices/UTCoreTypes.h>
 #import <MobileCoreServices/MobileCoreServices.h>
+#import "LivePhotoManager.h"
 
 // 自定义进度条视图类
 @interface DYYYDownloadProgressView : UIView
